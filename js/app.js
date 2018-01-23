f24ecfb4fc1139f9817a58de20f846f72e10d873
Merritt Lawrenson <merrittlawrenson@gmail.com>
2018-01-22 21:46:46 -0800

    added layer control

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 var routes;
 var stops;
 
-
 var map = L.map('map').fitBounds([
   [45.6077682, -122.9945375],
   [45.4289472, -122.4139835]
@@ -12,12 +11,26 @@ L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', 
   maxZoom: 18,
 }).addTo(map);
 
+var routeLayer = new L.LayerGroup().addTo(map);
+var stopLayer = new L.LayerGroup().addTo(map);
 
+var overlays = {
+  "Routes": routeLayer,
+  "Stops": stopLayer
+};
+
+L.control.layers(null, overlays).addTo(map);
 
 $.getJSON("./assets/tm_routes.geojson", function(routes) {
   routes = L.geoJSON(routes, {
     style: function (feature) {
-      return {color: "blue", opacity: 0.5};
+      console.log(feature);
+      return {
+        color: feature.properties.frequent === "True" ? "black" : "blue",
+        opacity: 0.5,
+        weight: feature.properties.frequent === "True" ? 4 : 2
+
+      };
     }
   }).bindPopup(function(feature) {
     return (
@@ -34,7 +47,7 @@ $.getJSON("./assets/tm_routes.geojson", function(routes) {
       "<br>Passenger Miles per Revenue Mile: " + feature.feature.properties.pass_miles_per_rev_mile_2017 + 
       "<br>Average Trip Length: " + feature.feature.properties.avg_trip_length_2017
     );
-  }).addTo(map);
+  }).addTo(routeLayer);
   $.getJSON("./assets/tm_stops.geojson", function(stops) {
     stops = L.geoJSON(stops, {
       pointToLayer: function (feature, latlng) {
@@ -44,7 +57,7 @@ $.getJSON("./assets/tm_routes.geojson", function(routes) {
         return {
           radius: feature.properties.total === null ? 5 : (Math.log10(parseInt(feature.properties.total.replace(/,/g, '')) + .01) * 2),
           // radius: feature.properties.total === null ? 5 : feature.properties.total == 0 ? .02 : parseInt(feature.properties.total.replace(/,/g, '')) / 250,
-          fillColor: feature.properties.total ? "#5174ad": "#aaa",
+          fillColor: feature.properties.total ? "#2d5596": "#ccc",
           color: "#000",
           weight: 1,
           opacity: 1,
@@ -53,6 +66,6 @@ $.getJSON("./assets/tm_routes.geojson", function(routes) {
       }
     }).bindPopup(function(feature) {
       return "<b>" + feature.feature.properties.stop_name + "</b><br>Stop ID: " + feature.feature.properties.stop_id + "<br><br>Ons: " + feature.feature.properties.ons + "<br>Offs: " + feature.feature.properties.offs + "<br><b>Total Riders: " + feature.feature.properties.total + "</b>";
-    }).addTo(map);   
+    }).addTo(stopLayer);   
   });
 });
