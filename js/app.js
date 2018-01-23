f17ea32f6c06d6b1e6196737f92353bf31f62d4d
Merritt Lawrenson <merrittlawrenson@gmail.com>
2018-01-22 21:26:37 -0800

    routes work too

--- js/app.js
+++ js/app.js
@@ -12,35 +12,47 @@ L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', 
   maxZoom: 18,
 }).addTo(map);
 
-// $.getJSON("./assets/tm_routes.geojson", function(routes) {
-//   routes = L.geoJSON(routes, {
-//     style: function (feature) {
-//       return {color: "black", opacity: 0.5};
-//     }
-//     // ,
-//     // filter: function (routeShape) {
-//     //   return markers[vehicle.vehicleID].routeNumber == routeShape.properties.rte && markers[vehicle.vehicleID].direction == routeShape.properties.dir;
-//     // }
-//   }).addTo(map);   
-// });
 
-$.getJSON("./assets/tm_stops.geojson", function(stops) {
-  stops = L.geoJSON(stops, {
-    pointToLayer: function (feature, latlng) {
-      return L.circleMarker(latlng);
-    },
+
+$.getJSON("./assets/tm_routes.geojson", function(routes) {
+  routes = L.geoJSON(routes, {
     style: function (feature) {
-      return {
-        radius: feature.properties.total === null ? 5 : (Math.log10(parseInt(feature.properties.total.replace(/,/g, '')) + .01) * 2),
-        // radius: feature.properties.total === null ? 5 : feature.properties.total == 0 ? .02 : parseInt(feature.properties.total.replace(/,/g, '')) / 250,
-        fillColor: feature.properties.total ? "#5174ad": "#aaa",
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-      };
+      return {color: "blue", opacity: 0.5};
     }
   }).bindPopup(function(feature) {
-    return "<b>" + feature.feature.properties.stop_name + "</b><br>Stop ID: " + feature.feature.properties.stop_id + "<br><br>Ons: " + feature.feature.properties.ons + "<br>Offs: " + feature.feature.properties.offs + "<br><b>Total Riders: " + feature.feature.properties.total + "</b>";
-  }).addTo(map);   
+    return (
+      "<b>" + feature.feature.properties.rte + " " + feature.feature.properties.rte_desc + 
+      "</b><br><br>Weekday Boardings 2017: " + feature.feature.properties.boarding_rides_2017 + 
+      "<br>Weekday Boardings 2016: " + feature.feature.properties.boarding_rides_2016 + 
+      "<br>Change 2016-2017: " + feature.feature.properties.change_boarding_rides + 
+      "<br><br>Riders per Vehicle Hour 2017: " + feature.feature.properties.rides_vehicle_hr_2017 + 
+      "<br>Riders per Vehicle Hour 2016: " + feature.feature.properties.rides_vehicle_hr_2016 + 
+      "<br>Change 2016-2017: " + feature.feature.properties.change_rides_vehicle_hr + 
+      "<br><br>Riders per Revenue Hour: " + feature.feature.properties.rides_rev_hr_2017 + 
+      "<br>Cost per Ride: " + feature.feature.properties.cost_per_ride_2017 + 
+      "<br>Passenger Miles: " + feature.feature.properties.passenger_miles_2017 + 
+      "<br>Passenger Miles per Revenue Mile: " + feature.feature.properties.pass_miles_per_rev_mile_2017 + 
+      "<br>Average Trip Length: " + feature.feature.properties.avg_trip_length_2017
+    );
+  }).addTo(map);
+  $.getJSON("./assets/tm_stops.geojson", function(stops) {
+    stops = L.geoJSON(stops, {
+      pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng);
+      },
+      style: function (feature) {
+        return {
+          radius: feature.properties.total === null ? 5 : (Math.log10(parseInt(feature.properties.total.replace(/,/g, '')) + .01) * 2),
+          // radius: feature.properties.total === null ? 5 : feature.properties.total == 0 ? .02 : parseInt(feature.properties.total.replace(/,/g, '')) / 250,
+          fillColor: feature.properties.total ? "#5174ad": "#aaa",
+          color: "#000",
+          weight: 1,
+          opacity: 1,
+          fillOpacity: 0.8
+        };
+      }
+    }).bindPopup(function(feature) {
+      return "<b>" + feature.feature.properties.stop_name + "</b><br>Stop ID: " + feature.feature.properties.stop_id + "<br><br>Ons: " + feature.feature.properties.ons + "<br>Offs: " + feature.feature.properties.offs + "<br><b>Total Riders: " + feature.feature.properties.total + "</b>";
+    }).addTo(map);   
+  });
 });
